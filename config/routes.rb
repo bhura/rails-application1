981fa6eef3b5b52ac11107f3e6301311e773a8c8
bhura <shrestha.bhuwan81@gmail.com>
2017-05-01 17:59:44 +1000

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/contact-us', to: 'static_pages#contact'
+
+  get '/about-us', to: 'static_pages#about'
+  get '/blog', to: 'static_pages#blog'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
